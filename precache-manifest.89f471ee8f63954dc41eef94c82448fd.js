0e0112f48e2e0ac25c9c52882313c3516b2ee0ae
Shivam Agrawal <shivam301296@gmail.com>
2020-07-17 15:45:10 +0000

    Updates

---
+++ precache-manifest.89f471ee8f63954dc41eef94c82448fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0de5ed69c039b21cc73383ba0f21769",
+    "url": "/3d/index.html"
+  },
+  {
+    "revision": "624267025cf230986df7",
+    "url": "/3d/static/css/main.ef509332.chunk.css"
+  },
+  {
+    "revision": "730eeabffcded9ee645d",
+    "url": "/3d/static/js/2.8fc0e0c0.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/3d/static/js/2.8fc0e0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624267025cf230986df7",
+    "url": "/3d/static/js/main.14ef71b9.chunk.js"
+  },
+  {
+    "revision": "3cd1361a8da90c9ae80e",
+    "url": "/3d/static/js/runtime-main.6cb2e245.js"
+  },
+  {
+    "revision": "da499b8537ee3ce0ed8469c0a73ecc2c",
+    "url": "/3d/static/media/crate.da499b85.gif"
+  }
+]);
